a2b34bd2bb300ed476d0fbd5a6e28cd0ffad3cd9
pcsafreire <pcsafreire@gmail.com>
2024-02-22 22:21:55 -0300

    Aula 22-01-20204 - criação inicial do projeto Spring Boot

---
+++ src/main/java/br/com/apifatec/apifatec/ApiFatecApplication.java
@@ -0,0 +1,13 @@
+package br.com.apifatec.apifatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFatecApplication.class, args);
+	}
+
+}
