8ecabcf15ba76cda1e25d76f80b8e51880209dc7
pcsafreire <pcsafreire@gmail.com>
2024-02-29 22:43:33 -0300

    aula 29-02-24 - endpoints iniciais

--- src/main/java/br/com/apifatec/apifatec/ApiFatecApplication.java
+++ src/main/java/br/com/apifatec/apifatec/ApiFatecApplication.java
@@ -2,10 +2,33 @@ package br.com.apifatec.apifatec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ApiFatecApplication {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/hello")
+	String home2 () {
+		return "Hello World 2";
+	}
+	
+	@RequestMapping("/numero")
+	Integer numero () {
+		return 9;
+	}
 
+	@RequestMapping("/numero/{num}")
+	Integer numero2 (@PathVariable Integer num) {
+		return num;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFatecApplication.class, args);
 	}
