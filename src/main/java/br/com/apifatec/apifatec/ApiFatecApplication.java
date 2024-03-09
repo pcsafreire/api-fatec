2ec1c4359af80110dc5c021d883b2d71aaac6d08
pcsafreire <pcsafreire@gmail.com>
2024-03-09 11:42:47 -0300

    Update 09/03

--- src/main/java/br/com/apifatec/apifatec/ApiFatecApplication.java
+++ src/main/java/br/com/apifatec/apifatec/ApiFatecApplication.java
@@ -1,6 +1,5 @@
 package br.com.apifatec.apifatec;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,39 +28,4 @@ public class ApiFatecApplication {
 	Integer numero2 (@PathVariable Integer num) {
 		return num;
 	}
-
-	@RequestMapping("/idade/{num}")
-	String idade(@PathVariable String num) {
-		int n;
-		
-		try{
-		     n = Integer.parseInt(num);
-		     
-		     if(n <= 0) {
-		    	 return "Idade inválida.";
-		     }
-				
-			 if( n < 12 ) {
-				 return "Criança";
-			 }
-			 else if (n < 19) {
-				 return "Adolescente";
-			 }
-			 else if(n < 61) {
-				 return "Adulto";
-			 }
-			 else {
-				return "Idoso";
-			}
-		}
-		catch (NumberFormatException ex){
-
-		}
-		    return "Idade inválida";
-		}
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiFatecApplication.class, args);
-	}
-
 }
--- src/main/java/br/com/apifatec/apifatec/controllers/exercícios/ExerciciosController.java
+++ src/main/java/br/com/apifatec/apifatec/controllers/exercícios/ExerciciosController.java
@@ -1,5 +1,41 @@
 package br.com.apifatec.apifatec.controllers.exercícios;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import br.com.apifatec.apifatec.ApiFatecApplication;
 
 public class ExerciciosController {
+@RequestMapping("/idade/{num}")
+	String idade(@PathVariable String num) {
+		int n;
+		
+		try{
+		     n = Integer.parseInt(num);
+		     
+		     if(n <= 0) {
+		    	 return "Idade inválida.";
+		     }
+				
+			 if( n < 12 ) {
+				 return "Criança";
+			 }
+			 else if (n < 19) {
+				 return "Adolescente";
+			 }
+			 else if(n < 61) {
+				 return "Adulto";
+			 }
+			 else {
+				return "Idoso";
+			}
+		}
+		catch (NumberFormatException ex){
 
+		}
+		    return "Idade inválida";
+		}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFatecApplication.class, args);
+	}
 }
