6b670acc02e26229052584dd94add580de01c202
pcsafreire <pcsafreire@gmail.com>
2024-04-04 22:09:10 -0300

    feat: :sparkles: Adicionar funcionalidade de atualização de cliente
    - Adiciona método na classe ClienteService para atualizar informações de um cliente existente. - Adiciona endpoint PUT no ClienteController para atualizar um cliente por ID.

--- src/main/java/br/com/apifatec/apifatec/controllers/exercícios/ClienteController.java
+++ src/main/java/br/com/apifatec/apifatec/controllers/exercícios/ClienteController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +61,12 @@ public class ClienteController {
 	public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
 		clienteService.deletarCliente(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> AtualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+		Cliente clienteAtualizado = clienteService.atualizarCliente(id, cliente);
+		return new ResponseEntity<>(clienteAtualizado, HttpStatus.OK);
 	}
 	
 }
--- src/main/java/br/com/apifatec/apifatec/domain/cliente/ClienteService.java
+++ src/main/java/br/com/apifatec/apifatec/domain/cliente/ClienteService.java
@@ -25,6 +25,29 @@ public class ClienteService {
 	}
 
 	public void deletarCliente(Long id) {
+
+		Cliente cliente = encontrarClientePorId(id);
+
+		if (cliente == null)
+		{
+			throw new IllegalArgumentException("Cliente não existe");
+		}	
 		clienteRepository.deleteById(id);
+
+	}
+
+		public Cliente atualizarCliente(Long id, Cliente cliente) {
+			Cliente clienteCadastradoCliente = encontrarClientePorId(id);
+		
+			if (clienteCadastradoCliente == null)
+			{
+				return null;
+			} else {
+				clienteCadastradoCliente.setNome(cliente.getNome());
+				clienteCadastradoCliente.setRazaoSocial(cliente.getRazaoSocial());
+				clienteCadastradoCliente.setEmail(cliente.getEmail());
+				clienteCadastradoCliente.setEndereco(cliente.getEndereco());
+				return clienteRepository.save(clienteCadastradoCliente);
+		}
 	}
 }
