aa4627e2aa9dd2849368cf7b79e96f83028c4572
pcsafreire <pcsafreire@gmail.com>
2024-04-11 21:51:26 -0300

    feat: :sparkles: Adicionar inicialização de dados de exemplo para Cliente e Produto
    - Adicionar CommandLineRunner para inserir clientes e produtos de exemplo ao iniciar a aplicação - Criar dados de exemplo para Cliente com nome, e-mail, endereço e razão social - Criar dados de exemplo para Produto com descrição, preço, quantidade em estoque e status de ativo

--- src/main/java/br/com/apifatec/apifatec/ApiFatecApplication.java
+++ src/main/java/br/com/apifatec/apifatec/ApiFatecApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import br.com.apifatec.apifatec.domain.cliente.ClienteRepository;
 import br.com.apifatec.apifatec.entities.Cliente;
+import br.com.apifatec.apifatec.domain.produto.ProdutoRepository;
 import br.com.apifatec.apifatec.entities.Produto;
 
 
@@ -15,7 +15,7 @@ import br.com.apifatec.apifatec.entities.Produto;
 @SpringBootApplication
 public class ApiFatecApplication {
 	@Bean
-	public CommandLineRunner run(@Autowired ClienteRepository clienteRepository){
+	public CommandLineRunner run(@Autowired ClienteRepository clienteRepository, ProdutoRepository produtoRepository){
 		return args -> {
 			Cliente cliente = new Cliente();
 			cliente.setNome("Paulo César");
@@ -24,16 +24,15 @@ public class ApiFatecApplication {
 			cliente.setRazaoSocial("Paulo");
 			clienteRepository.save(cliente);
 
-			// Inicializa alguns produtos de exemplo
             Produto produto1 = new Produto();
-            produto1.setDescricao("Produto 1");
+            produto1.setDescricao("Refrigerante");
             produto1.setPreco(10.0);
             produto1.setQuantidadeEstoque(100);
             produto1.setAtivo(true);
             produtoRepository.save(produto1);
 
             Produto produto2 = new Produto();
-            produto2.setDescricao("Produto 2");
+            produto2.setDescricao("Cerveja");
             produto2.setPreco(20.0);
             produto2.setQuantidadeEstoque(50);
             produto2.setAtivo(true);
--- src/main/java/br/com/apifatec/apifatec/entities/Produto.java
+++ src/main/java/br/com/apifatec/apifatec/entities/Produto.java
@@ -1,12 +1,11 @@
 package br.com.apifatec.apifatec.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.Email;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "produtos")
@@ -16,7 +15,7 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 60)
+    @Column(nullable = false, length = 70)
     private String descricao;
 
     @Column(nullable = false)
