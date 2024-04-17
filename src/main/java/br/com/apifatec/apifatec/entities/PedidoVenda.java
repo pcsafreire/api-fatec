4a7579624fb4811c56b901af33220fd28b2c5e88
pcsafreire <pcsafreire@gmail.com>
2024-04-17 19:46:53 -0300

    feat: :sparkles: Adicionar entidades PedidoVenda e PedidoVendaItem
    - Criar entidade PedidoVenda com atributos e relacionamentos adequados - Adicionar anotações de mapeamento para a tabela "pedido_venda" - Implementar entidade PedidoVendaItem com atributos e relacionamentos necessários - Incluir anotações de mapeamento para a tabela "pedido_venda_item"

---
+++ src/main/java/br/com/apifatec/apifatec/entities/PedidoVenda.java
@@ -0,0 +1,89 @@
+package br.com.apifatec.apifatec.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pedido_venda")
+public class PedidoVenda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @Column(name = "emissao")
+    private LocalDate emissao;
+
+    @Column(precision = 20, scale = 2)
+    private BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
+    private PedidoVendaStatusEnum status;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pedidoVenda", orphanRemoval = true)
+    private List<PedidoVendaItem> items = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getEmissao() {
+        return emissao;
+    }
+
+    public void setEmissao(LocalDate emissao) {
+        this.emissao = emissao;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public PedidoVendaStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(PedidoVendaStatusEnum status) {
+        this.status = status;
+    }
+
+    public List<PedidoVendaItem> getItems() {
+        return items;
+    }
+
+    public void addItem(PedidoVendaItem item) {
+        this.items.add(item);
+        item.setPedidoVenda(this);
+    }
+}
